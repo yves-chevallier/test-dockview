c56bf241bbd6d6c508787fd4cb98455b5ea3c65e
Yves Chevallier <yves.chevallier@heig-vd.ch>
2025-05-31 18:01:08 +0200

    Simplification

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,13 @@ import {
     DockviewApi,
     DockviewTheme,
 } from 'dockview';
-import * as React from 'react';
+import {MouseEvent, useRef, useEffect, useLayoutEffect, useState, createContext, useContext} from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './app.scss';
 import { defaultConfig } from './defaultLayout';
-import { GridActions } from './gridActions';
-import { PanelActions } from './panelActions';
-import { GroupActions } from './groupActions';
 import { LeftControls, PrefixHeaderControls, RightControls } from './controls';
-import { Table, usePanelApiMetadata } from './debugPanel';
 
-const DebugContext = React.createContext<boolean>(false);
-
-const Option = (props: {
-    title: string;
-    onClick: () => void;
-    value: string;
-}) => {
-    return (
-        <div>
-            <span>{`${props.title}: `}</span>
-            <button onClick={props.onClick}>{props.value}</button>
-        </div>
-    );
-};
+const DebugContext = createContext<boolean>(false);
 
 const ShadowIframe = (props: IDockviewPanelProps) => {
     return (
@@ -48,8 +31,7 @@ const ShadowIframe = (props: IDockviewPanelProps) => {
 
 const components = {
     default: (props: IDockviewPanelProps) => {
-        const isDebug = React.useContext(DebugContext);
-        const metadata = usePanelApiMetadata(props.api);
+        const isDebug = useContext(DebugContext);
 
         return (
             <div
@@ -74,29 +56,11 @@ const components = {
                 >
                     {props.api.title}
                 </span>
-
-                {isDebug && (
-                    <div style={{ fontSize: '0.8em' }}>
-                        <Option
-                            title="Panel Rendering Mode"
-                            value={metadata.renderer.value}
-                            onClick={() =>
-                                props.api.setRenderer(
-                                    props.api.renderer === 'always'
-                                        ? 'onlyWhenVisible'
-                                        : 'always'
-                                )
-                            }
-                        />
-
-                        <Table data={metadata} />
-                    </div>
-                )}
             </div>
         );
     },
     nested: (props: IDockviewPanelProps) => {
-        const theme = React.useContext(ThemeContext);
+        const theme = useContext(ThemeContext);
         return (
             <DockviewReact
                 components={components}
@@ -134,9 +98,9 @@ const components = {
         );
     },
     shadowDom: (props: IDockviewPanelProps) => {
-        const ref = React.useRef<HTMLDivElement>(null);
+        const ref = useRef<HTMLDivElement>(null);
 
-        React.useEffect(() => {
+        useEffect(() => {
             if (!ref.current) {
                 return;
             }
@@ -164,7 +128,7 @@ const components = {
 
 const headerComponents = {
     default: (props: IDockviewPanelHeaderProps) => {
-        const onContextMenu = (event: React.MouseEvent) => {
+        const onContextMenu = (event: MouseEvent) => {
             event.preventDefault();
             alert('context menu');
         };
@@ -172,119 +136,61 @@ const headerComponents = {
     },
 };
 
-const colors = [
-    'rgba(255,0,0,0.2)',
-    'rgba(0,255,0,0.2)',
-    'rgba(0,0,255,0.2)',
-    'rgba(255,255,0,0.2)',
-    'rgba(0,255,255,0.2)',
-    'rgba(255,0,255,0.2)',
-];
-let count = 0;
+const ThemeContext = createContext<DockviewTheme | undefined>(undefined);
 
-const WatermarkComponent = () => {
-    return <div>custom watermark</div>;
-};
-
-const ThemeContext = React.createContext<DockviewTheme | undefined>(undefined);
-
-const DockviewDemo = (props: { theme?: DockviewTheme }) => {
-    const [logLines, setLogLines] = React.useState<
-        { text: string; timestamp?: Date; backgroundColor?: string }[]
-    >([]);
-
-    const [panels, setPanels] = React.useState<string[]>([]);
-    const [groups, setGroups] = React.useState<string[]>([]);
-    const [api, setApi] = React.useState<DockviewApi>();
-
-    const [activePanel, setActivePanel] = React.useState<string>();
-    const [activeGroup, setActiveGroup] = React.useState<string>();
-
-    const [pending, setPending] = React.useState<
+export default (props: { theme?: DockviewTheme }) => {
+    const [api, setApi] = useState<DockviewApi>();
+    const [pending, setPending] = useState<
         { text: string; timestamp?: Date }[]
     >([]);
 
-    const addLogLine = (message: string) => {
-        setPending((line) => [
-            { text: message, timestamp: new Date() },
-            ...line,
-        ]);
-    };
-
-    React.useLayoutEffect(() => {
+    useLayoutEffect(() => {
         if (pending.length === 0) {
             return;
         }
-        const color = colors[count++ % colors.length];
-        setLogLines((lines) => [
-            ...pending.map((_) => ({ ..._, backgroundColor: color })),
-            ...lines,
-        ]);
         setPending([]);
     }, [pending]);
 
-    React.useEffect(() => {
+    useEffect(() => {
         if (!api) {
             return;
         }
 
         const disposables = [
             api.onDidAddPanel((event) => {
-                setPanels((_) => [..._, event.id]);
-                addLogLine(`Panel Added ${event.id}`);
+                console.log(`Panel Added ${event.id}`);
             }),
             api.onDidActivePanelChange((event) => {
-                setActivePanel(event?.id);
-                addLogLine(`Panel Activated ${event?.id}`);
+                console.log(`Panel Activated ${event?.id}`);
             }),
             api.onDidRemovePanel((event) => {
-                setPanels((_) => {
-                    const next = [..._];
-                    next.splice(
-                        next.findIndex((x) => x === event.id),
-                        1
-                    );
-
-                    return next;
-                });
-                addLogLine(`Panel Removed ${event.id}`);
+                console.log(`Panel Removed ${event.id}`);
             }),
 
             api.onDidAddGroup((event) => {
-                setGroups((_) => [..._, event.id]);
-                addLogLine(`Group Added ${event.id}`);
+                console.log(`Group Added ${event.id}`);
             }),
 
             api.onDidMovePanel((event) => {
-                addLogLine(`Panel Moved ${event.panel.id}`);
+                console.log(`Panel Moved ${event.panel.id}`);
             }),
 
             api.onDidMaximizedGroupChange((event) => {
-                addLogLine(
+                console.log(
                     `Group Maximized Changed ${event.group.api.id} [${event.isMaximized}]`
                 );
             }),
 
             api.onDidRemoveGroup((event) => {
-                setGroups((_) => {
-                    const next = [..._];
-                    next.splice(
-                        next.findIndex((x) => x === event.id),
-                        1
-                    );
-
-                    return next;
-                });
-                addLogLine(`Group Removed ${event.id}`);
+                console.log(`Group Removed ${event.id}`);
             }),
 
             api.onDidActiveGroupChange((event) => {
-                setActiveGroup(event?.id);
-                addLogLine(`Group Activated ${event?.id}`);
+                console.log(`Group Activated ${event?.id}`);
             }),
         ];
 
-        const loadLayout = () => {
+        (() => {
             const state = localStorage.getItem('dv-demo-state');
 
             if (state) {
@@ -298,9 +204,7 @@ const DockviewDemo = (props: { theme?: DockviewTheme }) => {
             }
 
             defaultConfig(api);
-        };
-
-        loadLayout();
+        })();
 
         return () => {
             disposables.forEach((disposable) => disposable.dispose());
@@ -311,24 +215,6 @@ const DockviewDemo = (props: { theme?: DockviewTheme }) => {
         setApi(event.api);
     };
 
-    const [watermark, setWatermark] = React.useState<boolean>(false);
-
-    const [gapCheck, setGapCheck] = React.useState<boolean>(false);
-
-    const css = React.useMemo(() => {
-        if (!gapCheck) {
-            return {};
-        }
-
-        return {
-            '--dv-group-gap-size': '0.5rem',
-            '--demo-border': '5px dashed purple',
-        } as React.CSSProperties;
-    }, [gapCheck]);
-
-    const [showLogs, setShowLogs] = React.useState<boolean>(false);
-    const [debug, setDebug] = React.useState<boolean>(false);
-
     return (
         <div
             className="dockview-demo"
@@ -337,197 +223,20 @@ const DockviewDemo = (props: { theme?: DockviewTheme }) => {
                 display: 'flex',
                 flexDirection: 'column',
                 flexGrow: 1,
-                padding: '8px',
-                backgroundColor: 'rgba(0,0,50,0.25)',
-                borderRadius: '8px',
-                position: 'relative',
-                ...css,
+                position: 'relative'
             }}
         >
-            <div>
-                <GridActions
-                    api={api}
-                    toggleCustomWatermark={() => setWatermark(!watermark)}
-                    hasCustomWatermark={watermark}
+            <ThemeContext.Provider value={props.theme}>
+                <DockviewReact
+                    components={components}
+                    defaultTabComponent={headerComponents.default}
+                    rightHeaderActionsComponent={RightControls}
+                    leftHeaderActionsComponent={LeftControls}
+                    prefixHeaderActionsComponent={PrefixHeaderControls}
+                    onReady={onReady}
+                    theme={props.theme}
                 />
-                {api && (
-                    <PanelActions
-                        api={api}
-                        panels={panels}
-                        activePanel={activePanel}
-                    />
-                )}
-                {api && (
-                    <GroupActions
-                        api={api}
-                        groups={groups}
-                        activeGroup={activeGroup}
-                    />
-                )}
-                {/* <div>
-                    <button
-                        onClick={() => {
-                            setGapCheck(!gapCheck);
-                        }}
-                    >
-                        {gapCheck ? 'Disable Gap Check' : 'Enable Gap Check'}
-                    </button>
-                </div> */}
-            </div>
-            <div
-                className="action-container"
-                style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    alignItems: 'center',
-                    padding: '4px',
-                }}
-            >
-                <button
-                    onClick={() => {
-                        setDebug(!debug);
-                    }}
-                >
-                    <span className="material-symbols-outlined">
-                        engineering
-                    </span>
-                </button>
-                {showLogs && (
-                    <button
-                        onClick={() => {
-                            setLogLines([]);
-                        }}
-                    >
-                        <span className="material-symbols-outlined">undo</span>
-                    </button>
-                )}
-                <button
-                    onClick={() => {
-                        setShowLogs(!showLogs);
-                    }}
-                >
-                    <span style={{ paddingRight: '4px' }}>
-                        {`${showLogs ? 'Hide' : 'Show'} Events Log`}
-                    </span>
-                    <span className="material-symbols-outlined">terminal</span>
-                </button>
-            </div>
-            <div
-                style={{
-                    flexGrow: 1,
-                    height: 0,
-                    display: 'flex',
-                }}
-            >
-                <div
-                    style={{
-                        flexGrow: 1,
-                        overflow: 'hidden',
-                        display: 'flex',
-                    }}
-                >
-                    <DebugContext.Provider value={debug}>
-                        <ThemeContext.Provider value={props.theme}>
-                            <DockviewReact
-                                components={components}
-                                defaultTabComponent={headerComponents.default}
-                                rightHeaderActionsComponent={RightControls}
-                                leftHeaderActionsComponent={LeftControls}
-                                prefixHeaderActionsComponent={
-                                    PrefixHeaderControls
-                                }
-                                watermarkComponent={
-                                    watermark ? WatermarkComponent : undefined
-                                }
-                                onReady={onReady}
-                                theme={props.theme}
-                            />
-                        </ThemeContext.Provider>
-                    </DebugContext.Provider>
-                </div>
-
-                {showLogs && (
-                    <div
-                        style={{
-                            width: '400px',
-                            backgroundColor: 'black',
-                            color: 'white',
-                            overflow: 'hidden',
-                            fontFamily: 'monospace',
-                            marginLeft: '10px',
-                            flexShrink: 0,
-                            display: 'flex',
-                            flexDirection: 'column',
-                        }}
-                    >
-                        <div style={{ flexGrow: 1, overflow: 'auto' }}>
-                            {logLines.map((line, i) => {
-                                return (
-                                    <div
-                                        style={{
-                                            height: '30px',
-                                            overflow: 'hidden',
-                                            textOverflow: 'ellipsis',
-                                            whiteSpace: 'nowrap',
-                                            fontSize: '13px',
-                                            display: 'flex',
-                                            alignItems: 'center',
-
-                                            backgroundColor:
-                                                line.backgroundColor,
-                                        }}
-                                        key={i}
-                                    >
-                                        <span
-                                            style={{
-                                                display: 'flex',
-                                                alignItems: 'center',
-                                                minWidth: '20px',
-                                                maxWidth: '20px',
-                                                color: 'gray',
-                                                borderRight: '1px solid gray',
-                                                marginRight: '4px',
-                                                paddingLeft: '4px',
-                                                height: '100%',
-                                            }}
-                                        >
-                                            {logLines.length - i}
-                                        </span>
-                                        <span>
-                                            {line.timestamp && (
-                                                <span
-                                                    style={{
-                                                        fontSize: '0.7em',
-                                                        padding: '0px 2px',
-                                                    }}
-                                                >
-                                                    {line.timestamp
-                                                        .toISOString()
-                                                        .substring(11, 23)}
-                                                </span>
-                                            )}
-                                            <span>{line.text}</span>
-                                        </span>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                        <div
-                            style={{
-                                padding: '4px',
-                                display: 'flex',
-                                justifyContent: 'flex-end',
-                            }}
-                        >
-                            <button onClick={() => setLogLines([])}>
-                                Clear
-                            </button>
-                        </div>
-                    </div>
-                )}
-            </div>
+            </ThemeContext.Provider>
         </div>
     );
 };
-
-export default DockviewDemo;
--- src/defaultLayout.ts
+++ src/defaultLayout.ts
@@ -2,7 +2,6 @@ import { DockviewApi } from 'dockview';
 
 export const nextId = (() => {
     let counter = 0;
-
     return () => counter++;
 })();
 
@@ -15,53 +14,10 @@ export function defaultConfig(api: DockviewApi) {
     });
 
     api.addPanel({
-        id: 'panel_2',
-        component: 'default',
-        title: 'Panel 2',
-        position: { referencePanel: panel1 },
-    });
-
-    api.addPanel({
-        id: 'panel_3',
-        component: 'default',
-        title: 'Panel 3',
-        position: { referencePanel: panel1 },
-    });
-
-    const panel4 = api.addPanel({
         id: 'panel_4',
         component: 'default',
         title: 'Panel 4',
         position: { referencePanel: panel1, direction: 'right' },
     });
-
-    const panel5 = api.addPanel({
-        id: 'panel_5',
-        component: 'default',
-        title: 'Panel 5',
-        position: { referencePanel: panel4 },
-    });
-
-    const panel6 = api.addPanel({
-        id: 'panel_6',
-        component: 'default',
-        title: 'Panel 6',
-        position: { referencePanel: panel5, direction: 'below' },
-    });
-
-    const panel7 = api.addPanel({
-        id: 'panel_7',
-        component: 'default',
-        title: 'Panel 7',
-        position: { referencePanel: panel6, direction: 'left' },
-    });
-
-    api.addPanel({
-        id: 'panel8',
-        component: 'default',
-        title: 'Panel 8',
-        position: { referencePanel: panel7, direction: 'below' },
-    });
-
     panel1.api.setActive();
 }
